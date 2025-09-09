693093e08716d8daf962d93d04c020d29bdbcb23
LiorKirshner <liorkk7@gmail.com>
2025-09-09 16:43:45 +0300

    Implement helper functions to read/write JSON file

---
+++ utils/recipesFile.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const RECIPES_PATH = path.join(__dirname, '../data/recipes.json');
+
+async function readRecipes() {
+  try {
+    const data = await fs.readFile(RECIPES_PATH, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    throw { statusCode: 500, message: 'Failed to read recipes file', original: err };
+  }
+}
+
+async function writeRecipes(recipes) {
+  try {
+    await fs.writeFile(RECIPES_PATH, JSON.stringify(recipes, null, 2), 'utf-8');
+  } catch (err) {
+    throw { statusCode: 500, message: 'Failed to write recipes file', original: err };
+  }
+}
+
+module.exports = { readRecipes, writeRecipes };
